efb081bab7624707b33d3c2a38c97c206382da50
Nilesh Gupta <nileshgupta2232>
2026-06-11 10:58:27 +0530

    emergency alerts

--- jwt_token.py
+++ jwt_token.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 SECRET_KEY     = os.getenv("SECRET_KEY")
 ALGORITHM      = os.getenv("ALGORITHM", "HS256")
-EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 120))
+EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 def create_access_token(data: dict) -> str:
     payload = data.copy()
