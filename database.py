f0b1ad4aa33145603e49afb1f24da5424d676408
Shubham2337 <chauguleshubham390@gmail.com>
2026-07-21 15:13:05 +0530

    solve all

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:Amaresh%40123@localhost:5432/mydb"
+DATABASE_URL = "postgresql://postgres:Admin%401234@localhost:5432/hospital_db"
 
 engine  = create_engine(DATABASE_URL)
 
