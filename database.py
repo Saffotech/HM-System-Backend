5316ec52f2b23453a503556dd1df82d6c0ab5be7
AmareshMaurya <amareshmaurya2005@gmail.com>
2026-08-12 13:37:32 +0530

    fix IPD bill payment flow

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:Admin%401234@localhost:5432/hospital_db"
+DATABASE_URL = "postgresql://postgres:amaresh@localhost:5432/Hospital"
 
 engine  = create_engine(DATABASE_URL)
 
